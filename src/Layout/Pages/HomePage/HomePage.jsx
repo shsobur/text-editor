2075231fceedf70c409616e9dc95a2c2f85964aa
shsobur <shsoburhossen951@gmail.com>
2024-12-28 22:11:46 +0600

    Re-edit home page design

--- src/Layout/Pages/HomePage/HomePage.jsx
+++ src/Layout/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
 import "./HomePage.css";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <>
       <div className="home_main_banner_container">
         <div className="home_banner_left_container">
-          <h2>Craft Your Words Effortlessly</h2>
+          <h2>Craft Your Words<br/> Effortlessly</h2>
           <p>
             Experience seamless editing with our rich text editor,
             <br /> designed to bring your ideas to life.
@@ -15,7 +15,6 @@ const HomePage = () => {
             <button>Get started</button>
           </Link>
         </div>
-        <div className="home_banner_right_container"></div>
       </div>
     </>
   );
