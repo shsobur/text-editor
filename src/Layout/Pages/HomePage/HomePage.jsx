b12a1e2e0bdd6d229b144d81284a58300dc05b39
shsobur <shsoburhossen951@gmail.com>
2024-12-28 21:45:04 +0600

    Home page design completed

--- src/Layout/Pages/HomePage/HomePage.jsx
+++ src/Layout/Pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./HomePage.css";
 
 const HomePage = () => {
@@ -5,12 +6,19 @@ const HomePage = () => {
     <>
       <div className="home_main_banner_container">
         <div className="home_banner_left_container">
-          Left
+          <h2>Craft Your Words Effortlessly</h2>
+          <p>
+            Experience seamless editing with our rich text editor,
+            <br /> designed to bring your ideas to life.
+          </p>
+          <Link to="/textEditor">
+            <button>Get started</button>
+          </Link>
         </div>
         <div className="home_banner_right_container"></div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
